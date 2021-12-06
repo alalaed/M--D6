05b52615534ec1534485e129adf11d1bfbbd0cde
Ali Alaeddine <a.alaeddine@live.com>
2021-12-06 09:20:12 +0100

    ex.2

---
+++ trial.js
@@ -0,0 +1,5 @@
+/* EXERCISE B
+    Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
+*/
+ 
+let sum = 10 + 11 + 12 +13 + 14 + 15 + 16 + 17 + 18 + 19 + 20
